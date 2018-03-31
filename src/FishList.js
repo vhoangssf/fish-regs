b4c657ee928a97fc05957d58aa57dd05589e2aa3
vhoangssf <vincenthoang00@gmail.com>
2018-03-31 09:09:49 -0700

    added modal to FishList

--- src/FishList.js
+++ src/FishList.js
@@ -1,29 +1,76 @@
 import React, { Component } from 'react';
-import { ListGroup, ListGroupItem, Media } from 'react-bootstrap';
+import { ListGroup, ListGroupItem, Media, Modal, Button } from 'react-bootstrap';
 import MockData from './MockData';
 
 class FishList extends Component {
+    constructor(props, context) {
+        super(props, context);
+
+        this.handleShow = this.handleShow.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+        this.state = {
+            show: false
+        };
+    }
+
+    handleClose() {
+        this.setState({ show: false });
+    }
+    handleShow() {
+        this.setState({ show: true });
+        console.log("fish clicked");
+    }
+
     render() {
         return (
-            <ListGroup>
-                <ListGroupItem>
-                    <Media>
-                        <Media.Left>
-                            <img width={64} height={64} src="/thumbnail.png" alt="thumbnail" />
-                        </Media.Left>
-                        <Media.Body>
-                            <Media.Heading>{MockData[0].name}</Media.Heading>
-                            <p>
-                                Size Limit: 14" Bag Limit: 5
-      </p>
-                        </Media.Body>
-                    </Media>
-                </ListGroupItem>
+            <div>
+                <Modal show={this.state.show} onHide={this.handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>{MockData[0].name}</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <h4>Scientific Name</h4>
+                        <p>{MockData[0].scientificName}</p>
+                        <h4>Size Limit</h4>
+                        <p>{MockData[0].size}</p>
+                        <h4>Bag Limit</h4>
+                        <p>{MockData[0].bagLimit}</p>
+                        <h4>Seasons</h4>
+                        <p>{MockData[0].seasons}</p>
+                        <h4>Locations</h4>
+                        <p>{MockData[0].location}</p>
+                        
+                        <hr />
+
+                        <h4>Description</h4>
+                        <p>{MockData[0].description}</p>
+                        
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button onClick={this.handleClose}>Close</Button>
+                    </Modal.Footer>
+                </Modal>
+
+                <ListGroup>
+                    <ListGroupItem onClick={this.handleShow}>
+                        <Media>
+                            <Media.Left>
+                                <img width={64} height={64} src="/thumbnail.png" alt="thumbnail" />
+                            </Media.Left>
+                            <Media.Body>
+                                <Media.Heading>{MockData[0].name}</Media.Heading>
+                                <p>
+                                    Size Limit: 14" Bag Limit: 5
+                                </p>
+                            </Media.Body>
+                        </Media>
+                    </ListGroupItem>
 
 
-                <ListGroupItem>Yellowtail</ListGroupItem>
-                <ListGroupItem>BlueFin</ListGroupItem>
-            </ListGroup>
+                    <ListGroupItem>Yellowtail</ListGroupItem>
+                    <ListGroupItem>BlueFin</ListGroupItem>
+                </ListGroup>
+            </div>
         );
     }
 }
--- src/MockData.js
+++ src/MockData.js
@@ -1,13 +1,23 @@
 const MockData = [
     {
         name: "Calico Bass",
+        scientificName: "Paralabrax clathratus",
         description: "The greatest fish tacos",
         size: 12,
         bagLimit: 5,
         seasons: "all",
         location: "kelp patties",
         tags: "#yellow monster"
-    }
+    },
+    {
+        name: "Yellowtail",
+        description: "Not a tuna",
+        size: 12,
+        bagLimit: 5,
+        seasons: "all",
+        location: "kelp patties",
+        tags: "#yoketail"
+    },
 ];
 
 export default MockData;
